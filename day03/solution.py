23a9586d8d85451b3782574caabb3ae972061320
Matt Moore <matt.d.moore@outlook.com>
2024-12-03 16:26:11 +0100

    Solve day 3

---
+++ day03/solution.py
@@ -0,0 +1,16 @@
+import re
+
+result = 0
+do = True
+with open("input.txt") as text:
+    for line in text:
+        pairs = re.findall("mul\((\d+),(\d+)\)|(do(n't)?\(\))", line)
+        for a, b, flag, _ in pairs:
+            match flag:
+                case "do()":
+                    do = True
+                case "don't()":
+                    do = False
+            if do and a:
+                result += int(a) * int(b)
+print(result)
